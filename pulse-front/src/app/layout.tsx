4c4748c3b10691bb4f5b72b8fbf023e32f53db8c
Mathis <delasalle.sio.lebreton.m@gmail.com>
2026-05-07 15:41:06 +0200

    Ajout component footer.tsx and layout.tsx

--- pulse-front/src/app/layout.tsx
+++ pulse-front/src/app/layout.tsx
@@ -2,34 +2,33 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from '../component/navbar';
+import Footer from "../component/footer";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Pulse"
-};
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-      <html lang="fr">
-      <body>
-      <Navbar />
-      <div className="pt-16">
-        {children}
-      </div>
-      </body>
-      </html>
-  );
+    return (
+        <html lang="fr">
+        <body>
+        <Navbar />
+        <div className="pt-16">
+            {children}
+        </div>
+        <Footer />
+        </body>
+        </html>
+    );
 }
