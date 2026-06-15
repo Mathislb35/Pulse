fa882776bc38328293e3fcdd1ca711ea93faa0a8
MaelSouv <souvestremael@gmail.com>
2026-06-15 11:24:13 +0200

    Liaison avec les communes

--- pulse-back/src/events/events.module.ts
+++ pulse-back/src/events/events.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
 import { Event } from './entities/event.entity';
+import { Commune } from '../communes/entities/commune.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Event])],
+  imports: [TypeOrmModule.forFeature([Event,Commune])],
   controllers: [EventsController],
   providers: [EventsService],
 })
